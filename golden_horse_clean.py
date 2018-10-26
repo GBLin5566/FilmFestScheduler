9414695a858b07c38e47beb3c54f1026c2d2f2c7
GBLin5566 <b00901056@ntu.edu.tw>
2018-10-27 01:42:20 +0800

    Able to produce clean output.csv

--- golden_horse_clean.py
+++ golden_horse_clean.py
@@ -7,11 +7,22 @@ REDUNDANT_INFO_LINE_NUM = 4
 TRAILING_USELESS_INFO_LINE_NUM = -1
 
 
+def clean_line(string, remove_trainling_position=-2):
+    return string.replace('\t', '').split(',')[:remove_trainling_position]
+
+
 def main():
     with open(args.input, encoding=args.encoding) as file_handle:
         lines = file_handle.readlines()[
             REDUNDANT_INFO_LINE_NUM:TRAILING_USELESS_INFO_LINE_NUM
         ]
+        cleaned_lines = [
+            clean_line(line) for line in lines
+        ]
+
+    with open(args.output, 'w') as file_handle:
+        for line in cleaned_lines:
+            file_handle.write(f'{",".join(line)}\n')
 
 
 if __name__ == '__main__':
